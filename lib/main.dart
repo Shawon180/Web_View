a3ed4eb158413d8756c6a138f18cb70b70400839
Shawon180 <shawon180410@gmail.com>
2022-01-22 22:54:04 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+void main (){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+       child: WebView(
+         javascriptMode: JavascriptMode.unrestricted,
+         initialUrl: 'https://www.canva.com/',
+       ),
+      ),
+    );
+  }
+}
